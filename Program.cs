a2bed65acd74babacc98dcdc9b8861cb66dce01c
Patricia Takehara <paty.takeharab@outlook.com>
2022-12-14 14:12:48 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿try //tente fazer algo
+{
+  string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
+
+  foreach (string linha in linhas)
+  {
+    Console.WriteLine(linha);
+  }
+}
+catch (FileNotFoundException ex) //catch => pegar obter
+{
+  Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não encontrado! {ex.Message}");
+}
+catch (DirectoryNotFoundException ex)
+{
+  Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Caminho da pasta não encontrado! {ex.Message}");
+}
+catch (Exception ex)
+{
+  Console.WriteLine($"Ocorreu uma exceção genérica! {ex.Message}");
+}
+finally
+{
+  Console.WriteLine("CHegou até awui!");
+}
+
+
+
